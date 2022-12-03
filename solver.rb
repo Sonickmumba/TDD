533e922a8f615ce900228b7fbbb5f3b5adcb88c1
sonickmumba <kingellie.mumba@gmail.com>
2022-12-03 06:16:44 +0200

    Test pass because ArgumentError is set

--- solver.rb
+++ solver.rb
@@ -1,6 +1,7 @@
 class Solver
   def factorial(num)
     return 1 if num.zero?
+    raise ArgumentError, 'This method only accepts 0 and positive integers' if num.negative?
 
     num * factorial(num - 1)
   end
--- spec/solver_spec.rb
+++ spec/solver_spec.rb
@@ -5,8 +5,8 @@ describe Solver do
       expect(num.factorial(0)).to eq(1)
     end
 
-    it "should only accept zero or positive integers" do
-      expect{Solver.factorial(-1)}.to raise_error(ArgumentError)
+    it 'should only accept zero or positive integers' do
+      expect { Solver.factorial(-1) }.to raise_error(ArgumentError)
     end
   end
 end
